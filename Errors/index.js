306832201511d55a45c94cba458e03ada1b52945
me <danielwong@Daniels-MacBook-Air-2.local>
2023-10-05 10:37:54 +0100

    changed EHM to use the error codes instead of status as pr advice from Kev

--- Errors/index.js
+++ Errors/index.js
@@ -1,14 +1,17 @@
 function handleCustomErrors(err, req, res, next) {
     if (err.status === 404) {
+        console.log("Hi, I'm here!")
         res.status(404).send({ msg: "Not Found" });
-    } 
-    next()
+    } else {
+    next(err)
+    }
 }
 function handlePSQLErrors(err, req, res, next) {
-    if (err.status === 400) {
+    if (err.code === "22P02") {
         res.status(400).send({ msg: "Bad Request" });
-    } 
-    next()
+    }  else {
+    next(err)
+    }
 }
 
 
