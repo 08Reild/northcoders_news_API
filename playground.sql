0f798682ab0075cf0f7475d07a21c4111f7c730c
me <danielwong@Daniels-MacBook-Air-2.local>
2023-10-05 14:34:35 +0100

    added playground to gitignore file

--- playground.sql
+++ playground.sql
@@ -4,44 +4,5 @@ SELECT *
 FROM articles
 LEFT JOIN articles ON articles.author = comments.author;
 
-
-
-
-
 SELECT * FROM articles;
 SELECT * FROM comments;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    -- SELECT articles.*,
-    -- COUNT(comments.comment_id) AS comment_count
-    -- FROM articles
-    -- LEFT JOIN comments 
-    -- ON articles.article_id = comments.article_id
-    -- GROUP BY articles.article_id
-    -- ORDER BY articles.created_at DESC;
-
-    -- SELECT * FROM comments
