869577b8d7e5960f4d7524621bab40766aae4d22
me <danielwong@Daniels-MacBook-Air-2.local>
2023-10-23 10:07:17 +0100

    added cors for project

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const {
     handle500Errors 
 } = require('./Errors/index.js')
 const app = express()
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
